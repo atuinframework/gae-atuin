c9c94915ddfbfc2faf0e4e0670206517891173e2
Paolo Casciello <paolo.casciello@gmail.com>
2013-10-11 13:45:30 +0200

    added babel, flask-babel, pytz

--- handler.py
+++ handler.py
@@ -4,7 +4,7 @@ sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
 
 from flask import Flask, g, request, url_for
 #from flask.ext.cache import Cache
-#from flask.ext.babel import Babel, get_locale as babel_get_locale
+from flask.ext.babel import Babel, get_locale as babel_get_locale
 
 import settings
 #import datastore
@@ -18,7 +18,7 @@ app.secret_key = settings.SECRET_KEY
 #auth.login_manager.setup_app(app)
 
 #Babel
-#babel = Babel(app)
+babel = Babel(app)
 
 #Cache
 # if settings.DEBUG:
@@ -47,50 +47,32 @@ def func():
 	except:
 	  pass
 	
-	# if settings.MULTILANGUAGE:
-	# 	g.babel = babel
-	# 	g.available_languages = settings.MULTILANGUAGE_LANGS
-	# 	g.language = babel_get_locale().language
-	# 	g.lurl_for = lurl_for
-	# else:
-	# 	g.lurl_for = url_for
+	if settings.MULTILANGUAGE:
+		g.babel = babel
+		g.available_languages = settings.MULTILANGUAGE_LANGS
+		g.language = babel_get_locale().language
+		g.lurl_for = lurl_for
+	else:
+		g.lurl_for = url_for
 
-# if settings.MULTILANGUAGE:
-# 	@babel.localeselector
-# 	def get_locale():
-# 		lang = request.path[1:].split('/', 1)[0]
-# 		if lang in settings.MULTILANGUAGE_LANGS:
-# 			return lang
-# 		else:
-# 			return 'en'
+if settings.MULTILANGUAGE:
+	@babel.localeselector
+	def get_locale():
+		lang = request.path[1:].split('/', 1)[0]
+		if lang in settings.MULTILANGUAGE_LANGS:
+			return lang
+		else:
+			return 'en'
 	
 
 @app.context_processor
 def inject_custom():
 	d = {
 			'SITE_TITLE': settings.SITE_TITLE,
-			#'lurl_for': g.lurl_for,
+			'lurl_for': g.lurl_for,
 		}
 	return d
 
 for (mount_position, mount_module) in settings.mounts:
 	app.register_blueprint(mount_module.bp, url_prefix=mount_position)
-	
-# if settings.NEWRELIC:
-# 	try:
-# 		import newrelic.agent
-# 		newrelic.agent.initialize(settings.NEWRELIC_CONFIG)
-# 		app2 = newrelic.agent.wsgi_application()(app)
-# 		#monkey patching
-# 		app2.run = app.run
-# 		app2.test_request_context = app.test_request_context
-# 		app = app2
-# 	except:
-# 		pass
-	
-# if settings.SENTRY_DSN:
-# 	from raven.contrib.flask import Sentry
-# 	app.config['SENTRY_DSN'] = settings.SENTRY_DSN
-# 	sentry = Sentry(app)
-
 	
