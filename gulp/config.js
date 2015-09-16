e8169f395c6f379d06c5211d1c9fece8f37e86aa
nyxgear <dev@nyxgear.com>
2015-09-16 15:41:09 +0200

    gulp config

--- gulp/config.js
+++ gulp/config.js
@@ -15,7 +15,21 @@ config ={
 	],
 	'img' : [
 		paths.static.src.img.root + '/**/*.@(jpg|gif|png|svg)'
-	]
+	],
+	'src' : {
+		'css' 		: [ paths.static.src.css.root + '/style.scss'	],
+		'css_admin' : [ paths.static.src.css.root + '/admin/style.scss'	],
+		'js' 		: [ paths.static.src.js.root + '/*.js'	],
+		'js_admin'	: [ paths.static.src.js.root + '/admin/*.js'	],
+		'img' 		: [ paths.static.src.img.root + '/**/*.@(jpg|gif|png|svg)' ]
+	},
+	'min' : {
+		'css' 		: paths.static.min.css.root,
+		'css_admin' : paths.static.min.css.root + '/admin',
+		'js' 		: paths.static.min.js.root,
+		'js_admin'  : paths.static.src.js.root + '/admin',
+		'img' 		: paths.static.src.img.root
+	}
 }
 
 module.exports = config;
