077b573e736fe6efb0ff5c08fdd7fc1081cdb778
nyxgear <dev@nyxgear.com>
2017-06-09 15:13:41 +0200

    settings.py moved to atuin folder

--- app/atuin/settings.py
+++ app/atuin/settings.py
@@ -6,16 +6,10 @@
 
 # basic configuration - these MUST BE present
 import config
-import datastore
 
 DEBUG = config.DEBUG
 SECRET_KEY = config.SECRET_KEY
 SITE_TITLE = config.SITE_TITLE
-
-try:
-	SQLALCHEMY_DATABASE_URI = config.SQLALCHEMY_DATABASE_URI
-except AttributeError:
-	SQLALCHEMY_DATABASE_URI = datastore.settings.SQLALCHEMY_DATABASE_URI
 
 try:
 	MULTILANGUAGE = config.MULTILANGUAGE
@@ -24,34 +18,14 @@ except AttributeError:
 	MULTILANGUAGE = False
 
 try:
-	NEWRELIC = config.NEWRELIC
-	NEWRELIC_CONFIG = config.NEWRELIC_CONFIG
-except AttributeError:
-	NEWRELIC = False
-	NEWRELIC_CONFIG = ''
-	
-try:
 	CACHE_CONFIG = config.CACHE_CONFIG
 except AttributeError:
 	CACHE_CONFIG = {'CACHE_TYPE': 'simple'}
-	
-try:
-	SENTRY_DSN = config.SENTRY_DSN
-except AttributeError:
-	SENTRY_DSN = False
 
 try:
-	S3_ACCESS_KEY_ID = config.S3_ACCESS_KEY_ID
-	S3_SECRET_ACCESS_KEY = config.S3_SECRET_ACCESS_KEY
-	S3_REGION_NAME = config.S3_REGION_NAME
-	S3_BUCKET_NAME = config.S3_BUCKET_NAME
+	FORCE_HTTPS = config.FORCE_HTTPS
 except AttributeError:
-	S3_ACCESS_KEY_ID = None
-	S3_SECRET_ACCESS_KEY = None
-	S3_REGION_NAME = None
-	S3_BUCKET_NAME = None
-
+	FORCE_HTTPS = False
 
 # apps mounts MUST BE present
 from urls import mounts
-
