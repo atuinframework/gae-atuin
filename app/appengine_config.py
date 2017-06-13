529f1f6981519ca8b3b2f1330c708e1ef029325b
nyxgear <dev@nyxgear.com>
2017-06-13 21:25:17 +0200

    moved appengine_config

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -1,22 +1,3 @@
 # -*- coding: utf-8 -*-
 """`appengine_config` gets loaded when starting a new application instance."""
-from atuin import vendor
-# insert `lib` as a site directory so our `main` module can load
-# third-party libraries, and override built-ins with newer
-# versions.
-vendor.add('../lib')
-
-def webapp_add_wsgi_middleware(app):
-	import gae_mini_profiler.profiler
-	app = gae_mini_profiler.profiler.ProfilerWSGIMiddleware(app)
-	return app
-
-def gae_mini_profiler_should_profile_production():
-	pass
-	from google.appengine.api import users
-	return users.is_current_user_admin()
-
-
-def gae_mini_profiler_should_profile_development():
-	pass
-	# return True
+import atuin.appengine_config
