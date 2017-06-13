475a38585fa86d58980279a8b26bdf4d92da7052
nyxgear <dev@nyxgear.com>
2017-06-13 20:21:50 +0200

    moved translations_js

--- app/atuin/js_translations/views.py
+++ app/atuin/js_translations/views.py
@@ -5,12 +5,8 @@ from flask import render_template, make_response
 
 bp = Blueprint('atuin.js_translations', __name__)
 
-@bp.route(u'it/js_translations.js', endpoint='index_it')
 @bp.route(u'en/js_translations.js', endpoint='index_en')
-@bp.route(u'de/js_translations.js', endpoint='index_de')
-@bp.route(u'fr/js_translations.js', endpoint='index_fr')
-@bp.route(u'es/js_translations.js', endpoint='index_es')
-@bp.route(u'js_translations.js', endpoint='index')
+@bp.route(u'js_translations.js')
 def index():
 	resp = make_response(render_template('atuin/js_translations/base.js'))
 	resp.headers['Content-Type'] = 'application/javascript'
