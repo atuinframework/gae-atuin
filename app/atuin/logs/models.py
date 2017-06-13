46c4bcbb8e3cf2cd1cc34ded35ab8d33edb33c8c
nyxgear <dev@nyxgear.com>
2017-06-13 12:30:20 +0200

    conversion to ndb of logs/models.py

--- app/atuin/logs/models.py
+++ app/atuin/logs/models.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
-import datetime
+from google.appengine.ext import ndb
 
-from atuin.datastore import db
-from atuin.datastore.types import JSONEncodedDict
 
-class Log(db.Model):
-	__tablename__ = 'logs'
-	
-	id = db.Column(db.Integer, primary_key=True)
-	timestamp = db.Column(db.DateTime, default=datetime.datetime.now, index=True)
-	
-	event_type = db.Column(db.String(30))
-	event_title =  db.Column(db.String(255))
-	event_data = db.Column(db.Text)
-	
-	__table_args__ = (		db.Index('ix_timestamp_event_type', 'timestamp', 'event_type'),
-								)
-	
+class Log(ndb.Model):
+	timestamp = ndb.DateTimeProperty('ts', auto_now_add=True, indexed=True)
+
+	event_type = ndb.StringProperty('ty', required=True, indexed=True)
+	event_title = ndb.StringProperty('ti')
+	event_data = ndb.TextProperty('d')
+
 	@classmethod
 	def log_event(cls, ev_type, ev_title, ev_data=''):
 		l = cls()
 		l.event_type = ev_type
 		l.event_title = ev_title
 		l.event_data = ev_data
-		
-		tmp_session = db.create_scoped_session()
-		tmp_session.add(l)
-		tmp_session.commit()
+
+		l.put()
