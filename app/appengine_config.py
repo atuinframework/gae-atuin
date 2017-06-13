30753053dcd64df7717d392f2ca3a04ff29b3f6d
nyxgear <dev@nyxgear.com>
2017-06-13 20:56:11 +0200

    vendor moved

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """`appengine_config` gets loaded when starting a new application instance."""
-import vendor
+from atuin import vendor
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
-vendor.add('lib')
+vendor.add('../lib')
 
 def webapp_add_wsgi_middleware(app):
 	import gae_mini_profiler.profiler
