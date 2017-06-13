2f8846172c8b8f9b9cd1996b9f25fdb78d5dd5c1
nyxgear <dev@nyxgear.com>
2017-06-13 13:47:32 +0200

    app.py

---
+++ app/app.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
+from atuin.handler import app
+app.jinja_loader.searchpath.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/templates')
+app.static_folder = os.path.dirname(os.path.realpath(__file__)) + '/static'
